9446399675454afcae753cb87837e069bb781209
pejman <naderpejman@yahoo.com>
2017-10-08 07:16:45 -0600

    Video32:WorkingwithTemplateandStyle

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -2,8 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styles: [
+      `
+      .author {
+        display: inline-block;
+        font-style: italic;
+        font-size:12px;
+        width:80%;
+      }
+
+      .config {
+        display: inline-block;
+        text-algn: right;
+        font-size:12px;
+        width:19%;
+      }
+
+
+      `
+    ]
 })
 export class AppComponent {
-    
+    content='pejman';
+    message={
+      content: 'A message',
+      author: 'Pejman',
+    };
 }
--- routes/app.js
+++ routes/app.js
@@ -3,24 +3,9 @@ var router = express.Router();
 var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc) {
-        if (err) {
-            return res.send('Error!');
-        }
-        res.render('node', {email: doc.email});
-    });
+  res.render('index');   //this will rendor the angular application
 });
 
-router.post('/', function(req, res, next) {
-    var email = req.body.email;
-    var user = new User({
-        firstName: 'Max',
-        lastName: 'Schwarz',
-        password: 'super-secret',
-        email: email
-    });
-    user.save();
-    res.redirect('/');
-});
+
 
 module.exports = router;
