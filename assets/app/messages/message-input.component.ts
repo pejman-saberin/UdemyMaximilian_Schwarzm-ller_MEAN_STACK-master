dd72e7dcaf725d1d6588b6c0b026604bb8a049b3
pejman <naderpejman@yahoo.com>
2017-10-08 18:53:57 -0600

    46:AddingMessagesWithAService
    Accessing a Service and adding values to the array in the service using services as a provider

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,15 +1,22 @@
 import {Component} from "@angular/core";
+import {MessageService} from "./message.service";
+import {Message} from "./message.model"
+
 
 @Component({
   selector: 'app-message-input',
   templateUrl: './message-input.component.html',
-
+  providers:[MessageService] //dependency injection
 
 })
 
 export class MessageInputComponent{
+  constructor(private messsageService:MessageService){}
   onSave(value:string){
-    console.log(value);
+    const message=new Message(value,'Max');
+    this.messsageService.addMessage(message);
+    //console.log(value);
+
   }
 
 }
--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -1,10 +1,12 @@
 import {Message} from "./message.model";
 
+
 export class MessageService{
   private messages:Message []=[];
 
   addMessage(message: Message){
     this.messages.push(message);
+    console.log(this.messages);
   }
 
   getMessage(){
