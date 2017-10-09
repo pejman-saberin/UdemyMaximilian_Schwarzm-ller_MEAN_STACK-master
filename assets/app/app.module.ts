96082a23c03f77d8fb9d89452948dec248e94797
pejman <naderpejman@yahoo.com>
2017-10-09 06:53:56 -0600

    55:CreatingUser-RelatedComponents
    Added Signin andSignup component

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -10,12 +10,19 @@ import { MessageInputComponent} from "./messages/message-input.component";
 import { MessagesComponent} from "./messages/messages.component";
 import { AuthenticationComponent} from "./auth/authentication.component";
 import {HeaderComponent} from "./header.component";
-import {routing} from "./app.routing"
+import {routing} from "./app.routing";
+import {LogoutComponent} from "./auth/Logout.component";
+import{SignupComponent} from  "./auth/Signup.component";
+import{SigninComponent} from  "./auth/Signin.component";
+
 
 
 @NgModule({
     declarations: [
         AppComponent, MessageComponent,MessageListComponent,MessageInputComponent,MessagesComponent,AuthenticationComponent,HeaderComponent
+        ,LogoutComponent
+        ,SignupComponent
+        ,SigninComponent
     ],
     imports: [BrowserModule,FormsModule,FormsModule,routing],
     bootstrap: [AppComponent]
