9fe4bd6659821363fc7fdc6f86983eb1b5eb92e2
pejman <naderpejman@yahoo.com>
2017-10-09 07:59:43 -0600

    57:User Signup with Reactive (Data-Driven) Forms
    Create your own form with the reacive approach

--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html'
+    selector: 'app-signup',
+    templateUrl: './signup.component.html'
 })
+export class SignupComponent implements OnInit {
+    myForm: FormGroup;
 
-export class SignupComponent{
- 
+    onSubmit() {
+        console.log(this.myForm);
+        console.log(this.myForm.pristine);  //if you get invalid values, it is because of asynch, this needs to be true
+        console.log(this.myForm.value.email);  // although you get inavalid the data is already submitted
+        this.myForm.reset();
+    }
+
+    ngOnInit() {
+        this.myForm = new FormGroup({
+            firstName: new FormControl(null, Validators.required),
+            lastName: new FormControl(null, Validators.required),
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+            ]),
+            password: new FormControl(null, Validators.required)
+        });
+    }
 }
