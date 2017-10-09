96fcf3df593d862c3736a553d573beae4c27cac0
pejman <naderpejman@yahoo.com>
2017-10-08 21:28:29 -0600

    52:SettingUpRouting

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -9,14 +9,15 @@ import { MessageListComponent} from "./messages/message-list.component";
 import { MessageInputComponent} from "./messages/message-input.component";
 import { MessagesComponent} from "./messages/messages.component";
 import { AuthenticationComponent} from "./auth/authentication.component";
-import {HeaderComponent} from "./header.component"
+import {HeaderComponent} from "./header.component";
+import {routing} from "./app.routing"
 
 
 @NgModule({
     declarations: [
         AppComponent, MessageComponent,MessageListComponent,MessageInputComponent,MessagesComponent,AuthenticationComponent,HeaderComponent
     ],
-    imports: [BrowserModule,FormsModule,FormsModule],
+    imports: [BrowserModule,FormsModule,FormsModule,routing],
     bootstrap: [AppComponent]
 })
 export class AppModule {
